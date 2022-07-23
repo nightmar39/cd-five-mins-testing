42178cdeff94deeb3e8758beba382370378f4d64
nightmar39 <anthonyzilla94@gmail.com>
2022-07-22 18:18:02 -0700

    adding files to local testing repo

---
+++ push/reverse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in ./argocd-definitions/dev/app*; do 
+	sed -i '' -e "s/nightmar39/GITHUB_USER/" -e "s/cd-five-mins/GITHUB_REPO/" -e "s/us-support-v2-cluster/CLUSTER_NAME/" $i
+done 
